dd28a8a72a0f6f4ea07c33d072e00aeb4cc87de3
Erik Quenon Steggall <esteggal@ucsc.edu>
2012-02-17 18:49:41 -0800

    Committing almost working server and client

--- asg3/client.c
+++ asg3/client.c
@@ -24,54 +24,154 @@
 #define MAXLINE 128
 #define MAXSEND 512
 
-FILE* dg_cli(FILE* fp, int sockfd, struct sockaddr* pservaddr, socklen_t servlen){
-    /*
-    int n;
-    char sendline[MAXLINE], recvline[MAXLINE+1];
+typedef struct server_info{
+    int fd;
+    char * port;
+    char * host_name;
+    struct sockaddr* server;
+    struct server_info * next;
+    struct server_info * prev;
+}server_info;
 
-    connect(sockfd, (struct sockaddr*)pservaddr, servlen);
-
-    while(fgets(sendline, MAXLINE, fp) != NULL){
-        write(sockfd, sendline, strlen(sendline)); //, 0, pservaddr, servlen);
-        n = read(sockfd, recvline, MAXLINE); //, 0, preply_addr, &len);
-
-        recvline[n] = 0;
-        fputs(recvline, stdout);
-    }
-    */
-    return fp;
+void chomp (char* string, char delim){
+    size_t len = strlen(string);
+    if(len == 0) return;
+    char *nlpos = string + len -1;
+    if(*nlpos == delim) *nlpos = '\0';
 }
 
 int main(int argc, char** argv){
     int sockfd;
     int n;
+    FILE* server_file;
     char sendline[MAXLINE], recvline[MAXLINE+1];
     struct sockaddr_in servaddr;
     struct sockaddr* pservaddr;
+    server_info* head;
+    server_info* tail;
+    server_info* temp;
     socklen_t servlen;
+    char* send_buffer;
+    int num_servers;
+    int server_counter;
+    int port_num;
+    int spin_cnt;
     FILE* fp;
 
 
-    if(argc != 2){
-        fprintf(stderr, "usage: >./server [port_number]\n");
+    if(argc != 3){
+        fprintf(stderr, "usage: >./client [request_file] [number of servers]\n");
         exit(1);
     }
-    bzero(&servaddr, sizeof(servaddr));
-    servaddr.sin_family = AF_INET;
-    servaddr.sin_port = htons(SERV_PORT);
-    inet_pton(AF_INET, argv[1], &servaddr.sin_addr);
-    sockfd = socket(AF_INET, SOCK_DGRAM, 0);
-    fp = dg_cli(stdin, sockfd, (struct sockaddr*) &servaddr, sizeof(servaddr));
-    pservaddr = (struct sockaddr*) &servaddr;
-    servlen = sizeof(servaddr);
-    connect(sockfd, (struct sockaddr*)pservaddr, servlen);
+    num_servers = atoi(argv[2]);
+    if(32 < strlen(argv[1])){
+        perror("request_file name too big\n");
+        exit(1);
+    }
+    send_buffer = calloc(1, (strlen(argv[1])+32)); 
 
-    while(fgets(sendline, MAXLINE, fp) != NULL){
-        write(sockfd, sendline, strlen(sendline)); //, 0, pservaddr, servlen);
-        n = read(sockfd, recvline, MAXLINE); //, 0, preply_addr, &len);
+    strncpy(send_buffer, argv[1], (strlen(argv[1])));
+    strcat(send_buffer, "\n");
+    if(64 < strlen(argv[2])){
+        perror("Number of servers is too high\n");
+        exit(1);
+    }
+    strcat(send_buffer, argv[2]);
+    strcat(send_buffer, "\n");
 
-        recvline[n] = 0;
-        fputs(recvline, stdout);
+    server_file = fopen("server-info.txt", "r");
+    if(server_file == NULL){
+        perror("Could not open server info file\n");
+        exit(1);
+    }
+
+    for(server_counter = 1;server_counter <= num_servers; server_counter++){
+        char buffer[MAXLINE];
+        char* fgets_rc = fgets(buffer, sizeof(buffer), server_file);
+        if(fgets_rc == NULL){
+            perror("Input not read\n");
+            exit(0);
+        }
+        chomp(buffer, '\n');
+        char* savepos = NULL;
+        char* bufptr = buffer;
+        server_info * new_server;
+        new_server = calloc(1, sizeof(server_info));
+        if(1 == server_counter){
+            tail = head = new_server;
+            new_server->prev = NULL;
+            new_server->next = NULL;
+        }else{
+            tail->next = new_server;
+            new_server->prev = tail;
+            new_server->next = NULL;
+            tail = new_server;
+        }
+        char* host = strtok_r(bufptr," \t\n", &savepos);
+        if(host == NULL) break;
+        new_server->host_name = calloc(1, 64);
+        strcpy(new_server->host_name, host);
+        bufptr = NULL;
+
+        char* port_str = strtok_r(bufptr," \t\n", &savepos);
+        if(port_str == NULL) break;
+        new_server->port = calloc(1, 8);
+        strcpy(new_server->port, port_str);
+        bufptr = NULL;
+//        if(server_counter > num_servers) break;
+    }
+
+
+    for(temp = head; temp != NULL; temp = temp->next){
+        if(NULL == temp->host_name)break;
+        if(NULL == temp->port)break;
+        printf("temp->host = %s\ntemp->port = %s\n", temp->host_name, temp->port);
+        struct sockaddr* servaddr_ptr;
+        port_num = atoi(temp->port);
+
+        bzero(&servaddr, sizeof(servaddr));
+        servaddr.sin_family = AF_INET;
+        servaddr.sin_port = htons(port_num);
+        inet_pton(AF_INET, temp->host_name, &servaddr.sin_addr);
+        temp->fd = socket(AF_INET, SOCK_DGRAM, 0);
+        servaddr_ptr = calloc(1, sizeof(servaddr));
+        servaddr_ptr = (struct sockaddr*) &servaddr;
+        temp->server = servaddr_ptr;
+        pservaddr = (struct sockaddr*) &servaddr;
+        servlen = sizeof(servaddr);
+        connect(temp->fd, (struct sockaddr*)pservaddr, servlen);
+    }
+
+    spin_cnt = 0;
+    for(temp = head; temp != NULL;){
+        if((temp->port == NULL) || (temp->host_name == NULL)){
+            printf("was null\n");
+            temp = head;
+        }
+        strcat(send_buffer, "a");
+        
+        write(temp->fd, send_buffer, strlen(send_buffer)); //, 0, pservaddr, servlen);
+
+        /*
+        while(fgets(sendline, MAXLINE, fp) != NULL){
+            write(temp->fd, sendline, strlen(sendline)); //, 0, pservaddr, servlen);
+            */
+        while(1){
+            n = read(temp->fd, recvline, MAXLINE); //, 0, preply_addr, &len);
+    
+            recvline[n] = 0;
+            fputs(recvline, stdout);
+            spin_cnt++;
+            printf("spin count = %d\n", spin_cnt);
+            break;
+        }
+        if(temp == tail){
+            temp = head;
+        }
+        else{
+            temp = temp->next;
+        }
+        if(num_servers <= spin_cnt)break;
     }
     return 0;
 }
--- asg3/server.c
+++ asg3/server.c
@@ -25,27 +25,34 @@
 #define MAXSEND 512
 
 
-void dg_echo(int sockfd, struct sockaddr * pcliaddr, socklen_t clilen){
-    int n;
-    socklen_t len;
-    char mesg[MAXLINE];
-
-    for(;;){
-        bzero(&mesg, sizeof(mesg));
-        len = clilen;
-        n = recvfrom(sockfd, mesg, MAXLINE, 0, pcliaddr, &len);
-        printf("msg = %s\n", mesg);
-        sendto(sockfd, mesg, n, 0, pcliaddr, len);
-    }
-}
-
 int main(int argc, char** argv){
     int sockfd;
     int n;
     socklen_t len, clilen;
     char mesg[MAXLINE];
+    char buff[MAXSEND];
     struct sockaddr_in servaddr, cliaddr;
     struct sockaddr * pcliaddr;
+    int port_num;
+    FILE* request_file;
+    int num_servers;
+    int offset;
+    long actual_offset;
+    long file_size;
+    size_t bytes_to_read;
+    size_t bytes_read;
+    char* bufptr;
+    char* savepos = NULL;
+
+
+
+
+    if(argc != 2){
+        fprintf(stderr, "usage: >./server [port_num] \n");
+        exit(1);
+    }
+
+    port_num = atoi(argv[1]);
 
     sockfd = socket(AF_INET, SOCK_DGRAM, 0);
 
@@ -53,17 +60,62 @@ int main(int argc, char** argv){
 
     servaddr.sin_family = AF_INET;
     servaddr.sin_addr.s_addr = htonl(INADDR_ANY);
-    servaddr.sin_port = htons(SERV_PORT);
+    servaddr.sin_port = htons(port_num);
 
     bind(sockfd, (struct sockaddr*) &servaddr, sizeof(servaddr));
-//    dg_echo(sockfd, (struct sockaddr*) &cliaddr, sizeof(cliaddr));
     pcliaddr = (struct sockaddr*) &cliaddr;
     clilen = sizeof(cliaddr);
     for(;;){
         bzero(&mesg, sizeof(mesg));
         len = clilen;
         n = recvfrom(sockfd, mesg, MAXLINE, 0, pcliaddr, &len);
-        printf("msg = %s\n", mesg);
+        bufptr = mesg;
+        char* file_name = strtok_r(bufptr," \n\t\0", &savepos);
+        if(NULL == file_name){
+            perror("No file name input\n");
+            exit(1);
+        }
+        bufptr = NULL;
+
+        char* num_servers_str = strtok_r(bufptr," \n\t\0", &savepos);
+        if(NULL == num_servers_str){
+            perror("No file name input\n");
+            exit(1);
+        }
+        bufptr = NULL;
+
+        char* offset_str = strtok_r(bufptr," \n\t\0", &savepos);
+        if(NULL == offset_str){
+            perror("No file name input\n");
+            exit(1);
+        }
+        bufptr = NULL;
+
+
+
+        offset = (strlen(offset_str)-1);
+        num_servers = atoi(num_servers_str);
+        offset = offset%num_servers;
+        request_file = fopen(file_name, "r");
+
+
+        fseek(request_file, 0, SEEK_END);
+        file_size = ftell(request_file);
+        bytes_to_read = (size_t) file_size/num_servers;
+        actual_offset = (long) offset*bytes_to_read;
+
+        fseek(request_file, actual_offset, SEEK_SET);
+
+        bytes_read = fread(buff,1,bytes_to_read,request_file);
+        if(bytes_read > bytes_to_read || bytes_read < 0){
+            perror("Error reading file\n");
+            close(sockfd);
+            exit(1);
+        }
+        buff[bytes_read] = '\n';
+        printf("Read in %s\n", buff);
+
+
         sendto(sockfd, mesg, n, 0, pcliaddr, len);
     }
 }
