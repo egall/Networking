0c9046ebf049fe41bbcfe2edf8eb6dc7716beaf5
Erik Steggall <esteggall@soe.ucsc.edu>
2012-03-11 16:34:30 -0700

    Got http_proxy() working

--- browser_wrapper/http_proxy.c
+++ browser_wrapper/http_proxy.c
@@ -50,106 +50,48 @@ void sig_child(int signo){
 }
 
 static void
-proxy_http( char* method, char* path, char* protocol, FILE* sockrfp, FILE* sockwfp )
-    {
-    printf("##### proxy_http ####\n");
-    printf("method = %s\n", method);
-    printf("path = %s\n", path);
-    printf("protocol = %s\n", protocol);
-    char line[10000], protocol2[10000], comment[10000];
-    int first_line, status, ich;
-    long content_length, i;
-
-    /* Send request. */
-    (void) fprintf( sockwfp, "%s %s %s\r\n", method, path, protocol );
-    content_length = -1;
-    first_line = 1;
-    status = -1;
-    while ( fgets( line, sizeof(line), sockrfp ) != (char*) 0 ){
-	if ( strcmp( line, "\n" ) == 0 || strcmp( line, "\r\n" ) == 0 )
-	    break;
-	(void) fputs( line, stdout );
-//	(void) alarm( TIMEOUT );
-	trim( line );
-	if ( first_line )
-	    {
-	    (void) sscanf( line, "%[^ ] %d %s", protocol2, &status, comment );
-	    first_line = 0;
-	    }
-	if ( strncasecmp( line, "Content-Length:", 15 ) == 0 )
-	    content_length = atol( &(line[15]) );
+proxy_http(int remotefd, char* method, char* url, char* protocol){
+    char send_buff[1000];
+    char buffer[200];
+    char recv_data[2000];
+    strncpy(send_buff, method, strlen(method));
+    strcat(send_buff, " ");
+    strcat(send_buff, url);
+    strcat(send_buff, " ");
+    strcat(send_buff, protocol);
+    strcat(send_buff, "\n\n");
+//    printf("send buff = %s\n", send_buff);
+    printf("Connected to remote server\n");
+    write(remotefd, send_buff, sizeof(send_buff));
+    printf("Sent header\n");
+    char *tok = NULL;
+    int bufpos = 0;
+    int bytesread = 0;
+    bytesread = recv(remotefd, buffer, sizeof(buffer), 0);
+    if(bytesread){
+        printf("Data received from server: \n");
+        strncpy(&recv_data[bufpos], buffer, bytesread);
+        printf("%s\n", recv_data);
     }
-    /* Add a response header. */
-    (void) fputs( "Connection: close\r\n", stdout );
-    (void) fputs( line, stdout );
-    (void) fflush( stdout );
-    /* Under certain circumstances we don't look for the contents, even
-    ** if there was a Content-Length.
-    */
-    if ( strcasecmp( method, "HEAD" ) != 0 && status != 304 )
-	{
-	/* Forward the content too, either counted or until EOF. */
-	for ( i = 0;
-	      ( content_length == -1 || i < content_length ) && ( ich = getc( sockrfp ) ) != EOF;
-	      ++i )
-	    {
-	    putchar( ich );
-	    if ( i % 10000 == 0 )
-                printf("IDK\n");
-//		(void) alarm( TIMEOUT );
-	    }
-	}
-    (void) fflush( stdout );
+    else{
+        printf("couldn't read bytes\n");
     }
-
-
-
-
-/*
-static int open_remote_sock(char* hostname, int port, char* forward){
-    socklen_t remote_len;
-    struct sockaddr_in remoteaddr;
-    char buff[1000];
-    int numbytes;
-    int ptonret;
-    remote_len = sizeof(remoteaddr);
-    struct hostent *he;
-    int sock_family, sock_type, sock_protocol;
-    int sockfd;
-    printf("opening remote server socket...\n");
-
-    he = gethostbyname("localhost");
-//    he = gethostbyname("www.example.com");
-
-    sockfd = socket(AF_INET, SOCK_STREAM, 0);
-    if(sockfd < 0) perror("Internal Error: Couldn't create socket.\n");
-
-    printf("port = %d\nhostname = %s\n", port, hostname);
-    memset(&remoteaddr, 0, sizeof(remoteaddr));
-    remoteaddr.sin_family = AF_INET;
-    remoteaddr.sin_port = htons(REMOTE_PORT);
-    remoteaddr.sin_addr.s_addr = inet_addr("192.0.43.10");
-//    remoteaddr.sin_port = htons(port);
-//    ptonret = inet_pton(AF_INET, he->h_addr, &remoteaddr.sin_addr);
-    if(ptonret < 0)
-      perror("Can't set inet_pton\n");
     
-    if( connect(sockfd, (struct sockaddr*) &remoteaddr, remote_len) < 0)
-        perror("Service unavailable: connection refused\n");
-     
- 
-    return sockfd;
-} 
-*/
+}
 
-static int open_remote_sock(char* hostname, int port, char* forward){
+
+
+static int open_remote_sock(char* hostname, int port, char* method, char* url, char* protocol){
     int sock;
     struct sockaddr_in remote_addr;
     char* server_ip;
     int port_num;
     char buffer[200];
     char data[2000];
+    char send_buff[1000];
     uint32_t search_count = 0;
+    struct hostent *hent;
+    char* remote_ip;
     server_ip = "192.0.43.10";
     port_num = 80;
 
@@ -158,18 +100,31 @@ static int open_remote_sock(char* hostname, int port, char* forward){
         exit(-2);
     }
     printf("Socket created\n");
+
+/*
+    strncpy(send_buff, method, strlen(method));
+    strcat(send_buff, " ");
+    strcat(send_buff, url);
+    strcat(send_buff, " ");
+    strcat(send_buff, protocol);
+    strcat(send_buff, "\n\n");
+*/
+
+    hent = gethostbyname(hostname);
+    remote_ip = inet_ntoa(*((struct in_addr*) hent->h_addr));
     
     memset(&remote_addr, 0, sizeof(remote_addr));
     remote_addr.sin_family = AF_INET;
-    remote_addr.sin_addr.s_addr = inet_addr(server_ip);
+    remote_addr.sin_addr.s_addr = inet_addr(remote_ip);
     remote_addr.sin_port = htons(port);
 
     if(connect(sock, (struct sockaddr*) &remote_addr, sizeof(remote_addr)) < 0){
         perror("Error: failed to connect to server\n");
         exit(-3);
     }
+/*
     printf("Connected to remote server\n");
-    write(sock, HTTP_HEADER, sizeof(HTTP_HEADER));
+    write(sock, send_buff, sizeof(HTTP_HEADER));
     printf("Sent header\n");
     char *tok = NULL;
     int bufpos = 0;
@@ -183,7 +138,8 @@ static int open_remote_sock(char* hostname, int port, char* forward){
     else{
         printf("couldn't read bytes\n");
     }
-    return 0;
+*/
+    return sock;
 }
 
 void get_request(int sockfd){
@@ -191,13 +147,12 @@ void get_request(int sockfd){
     int port;
     int numbytes;
     pid_t childpid;
-    char line[1000], method[1000], url[1000], protocol[1000], host[1000], path[1000], buff[1000], forward[1000];
+    char line[1000], method[1000], url[1000], protocol[1000], host[1000], path[1000], buff[1000];
     int iport;
     while((n = read(sockfd, line, MAXLINE)) > 0){
         if(n < 0) perror("read failed\n");
-        strcpy(forward, line);
         trim(line);
-        if(sscanf(line, "%[^ ] %[^ ] %[^ ]", method, url, protocol) != 3)
+        if(sscanf(line, "%[^ ] %[^ ] %[^ \n]", method, url, protocol) != 3)
             perror("Bad request: Can't parse request\n");
         if( url == (char*) 0)
             perror("Bad request: NULL url\n");
@@ -230,31 +185,9 @@ void get_request(int sockfd){
         }
         if( (childpid = fork()) == 0){
             int remoteservefd = 0;
-//            FILE* remoterfp;
-//            FILE* remotewfp;
-
-            remoteservefd = open_remote_sock(host, port, forward);
-//            remoterfp = fdopen(remoteservefd, "r");
-//            remotewfp = fdopen(remoteservefd, "w");
-//            numbytes = send(remoteservefd, forward, sizeof(forward), 0); 
-/*
-            numbytes = send(remoteservefd, forward, strlen(forward), 0);
-            printf("numbytes = %d\n", numbytes);
-            if(numbytes < 0){
-               perror("Didn't send bytes\n");
-               exit(0);
-            }
-            numbytes = recv(remoteservefd, buff, sizeof(buff), 0);
-            printf("numbytes = %d\n", numbytes);
-            printf("Data = %s\n", buff);
-            if(numbytes < 0){
-            printf("##############################################\n");
-               perror("Didn't receive bytes\n");
-               exit(0);
-            }
-*/
-//            proxy_http(method, path, protocol, remoterfp, remotewfp);
-            printf("remote server fd = %d\n", remoteservefd);
+            remoteservefd = open_remote_sock(host, port, method, url, protocol);
+            printf("################# proxy_http\n");
+            proxy_http(remoteservefd, method, url, protocol);
             close(remoteservefd);
             exit(0);
         }
