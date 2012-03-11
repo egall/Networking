dd4f98ed87b742ef22adc5546e434a49d4f03df1
Erik Steggall <esteggall@soe.ucsc.edu>
2012-03-11 16:59:00 -0700

    It's getting the request, sending the request, receiving the request, but not sending it back to the browser

--- browser_wrapper/http_proxy.c
+++ browser_wrapper/http_proxy.c
@@ -49,33 +49,34 @@ void sig_child(int signo){
     return;
 }
 
-static void
+char*
 proxy_http(int remotefd, char* method, char* url, char* protocol){
     char send_buff[1000];
     char buffer[200];
-    char recv_data[2000];
+    char* recv_data;
     strncpy(send_buff, method, strlen(method));
     strcat(send_buff, " ");
     strcat(send_buff, url);
     strcat(send_buff, " ");
     strcat(send_buff, protocol);
     strcat(send_buff, "\n\n");
-//    printf("send buff = %s\n", send_buff);
     printf("Connected to remote server\n");
     write(remotefd, send_buff, sizeof(send_buff));
-    printf("Sent header\n");
+    printf("Sent header:\n%s\n", send_buff);
     char *tok = NULL;
     int bufpos = 0;
     int bytesread = 0;
     bytesread = recv(remotefd, buffer, sizeof(buffer), 0);
     if(bytesread){
         printf("Data received from server: \n");
+        recv_data = calloc(1, bytesread+1);
         strncpy(&recv_data[bufpos], buffer, bytesread);
-        printf("%s\n", recv_data);
     }
     else{
-        printf("couldn't read bytes\n");
+        perror("Couldn't read bytes\n");
+        exit(0);
     }
+    return recv_data;
     
 }
 
@@ -101,15 +102,6 @@ static int open_remote_sock(char* hostname, int port, char* method, char* url, c
     }
     printf("Socket created\n");
 
-/*
-    strncpy(send_buff, method, strlen(method));
-    strcat(send_buff, " ");
-    strcat(send_buff, url);
-    strcat(send_buff, " ");
-    strcat(send_buff, protocol);
-    strcat(send_buff, "\n\n");
-*/
-
     hent = gethostbyname(hostname);
     remote_ip = inet_ntoa(*((struct in_addr*) hent->h_addr));
     
@@ -122,32 +114,17 @@ static int open_remote_sock(char* hostname, int port, char* method, char* url, c
         perror("Error: failed to connect to server\n");
         exit(-3);
     }
-/*
-    printf("Connected to remote server\n");
-    write(sock, send_buff, sizeof(HTTP_HEADER));
-    printf("Sent header\n");
-    char *tok = NULL;
-    int bufpos = 0;
-    int bytesread = 0;
-    bytesread = recv(sock, buffer, sizeof(buffer), 0);
-    if(bytesread){
-        printf("Data received from server\n");
-        strncpy(&data[bufpos], buffer, bytesread);
-        printf("%s\n", data);
-    }
-    else{
-        printf("couldn't read bytes\n");
-    }
-*/
     return sock;
 }
 
-void get_request(int sockfd){
+char*
+get_request(int sockfd){
     ssize_t n;
     int port;
     int numbytes;
     pid_t childpid;
     char line[1000], method[1000], url[1000], protocol[1000], host[1000], path[1000], buff[1000];
+    char* recved_data;
     int iport;
     while((n = read(sockfd, line, MAXLINE)) > 0){
         if(n < 0) perror("read failed\n");
@@ -186,14 +163,16 @@ void get_request(int sockfd){
         if( (childpid = fork()) == 0){
             int remoteservefd = 0;
             remoteservefd = open_remote_sock(host, port, method, url, protocol);
-            printf("################# proxy_http\n");
-            proxy_http(remoteservefd, method, url, protocol);
+            recved_data = proxy_http(remoteservefd, method, url, protocol);
+//            printf("%s", recved_data);
             close(remoteservefd);
+            return recved_data;
             exit(0);
         }
 
 
     }
+    return NULL;
 }
 
 void str_echo(int sockfd){
@@ -211,6 +190,7 @@ int main(int argc, char** argv){
     socklen_t browser_len;
     struct sockaddr_in browseraddr, servaddr;
     void sig_child(int);
+    char* ret_data;
     int port_num;
 /*
     if(argv[1] == NULL){
@@ -238,7 +218,10 @@ int main(int argc, char** argv){
         if( (childpid = fork()) == 0){
             close(listenfd);
 
-            get_request(connfd);
+            ret_data = get_request(connfd);
+            printf("Returned data: \n");
+            printf("\n%s\n", ret_data);
+            write(connfd, ret_data, sizeof(ret_data));
             exit(0);
       }
         close(connfd);
